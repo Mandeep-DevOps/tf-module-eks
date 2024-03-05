dc7071ee026d919cb55671bf93e6827388ae7995
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-05 08:36:18 +0530

    IAM Correction

--- main.tf
+++ main.tf
@@ -44,17 +44,17 @@ resource "aws_iam_role" "node" {
 
 resource "aws_iam_role_policy_attachment" "main-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.main.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "main-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.main.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "main-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.main.name
+  role       = aws_iam_role.node.name
 }
 
 
