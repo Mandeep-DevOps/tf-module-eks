87a88967c333ae5c8472097bf9305c95166cfc0e
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-01 08:06:56 +0530

    Create one Pod Role

---
+++ iam-roles-for-pods.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "external-dns" {
+  name = "${local.name}-node-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Principal": {
+          "Federated": "arn:aws:iam::739561048503:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/425BA7CE0D854F12026FA6444CF6F673"
+        },
+        "Condition": {
+          "StringEquals": {
+            "oidc.eks.us-east-1.amazonaws.com/id/425BA7CE0D854F12026FA6444CF6F673:aud": "sts.amazonaws.com"
+            "oidc.eks.us-east-1.amazonaws.com/id/425BA7CE0D854F12026FA6444CF6F673:aud": "system:serviceaccount:default:external-dns"
+          }
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "parameter-store"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "Route53Access",
+          "Effect" : "Allow",
+          "Action" : [
+            "route53:*"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+}
+
